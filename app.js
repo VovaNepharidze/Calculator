c4689ea99ec7df3ed7828a91226cdb25beb70bfe
Vova Nepharidze <vova_n@yahoo.com>
2023-02-06 13:44:23 +0400

    adding calc files

---
+++ app.js
@@ -0,0 +1,60 @@
+let input = document.getElementById("screen")
+
+let numbers = document.querySelectorAll(".number")
+let operators = document.querySelectorAll(".operator")
+
+let buttonClearAll = document.querySelector("#clear-all")
+let buttonClear = document.querySelector("#clear")
+let buttonEqual= document.getElementById("equal")
+
+let operator = ""
+let num1 = "" 
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click",function(){
+        input.value += numbers[i].innerHTML
+    })
+}
+for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener("click",function(){
+        operator = operators[i].innerHTML
+        num1 =Number(input.value) 
+        input.value = ''
+    })
+}
+
+buttonClearAll.addEventListener("click",function(){
+    input.value= ""
+})
+buttonClear.addEventListener("click",function(){
+    input.value= input.value.slice(0,-1);
+})
+
+buttonEqual.addEventListener('click',function(){
+    let num2 = Number(input.value)
+    let result;
+
+    switch(operator){
+        case "+" :
+        result = num1 + num2
+            break;
+            case "-" :
+        result = num1 - num2
+            break;
+            case "/" :
+        result = num1 / num2
+            break;
+            case "*" :
+         result = num1 * num2
+            break;
+            case "%" :
+        result = num1 % num2
+            break;
+
+    }
+    input.value = result
+})
+
+
+
+
